5909c3b5becb0f2664d99708b588746589257284
Liu Cheng 19cl14 <1942839435@qq.com>
2022-02-26 17:39:31 -0500

    LC

--- 226 A2/Assets/Scripts/Timer.cs
+++ 226 A2/Assets/Scripts/Timer.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Timer : MonoBehaviour
 {
@@ -31,7 +32,7 @@ public class Timer : MonoBehaviour
         }
         else
         {
-            playerTransForm.position = respawn.position;
+            SceneManager.LoadScene("Fail");
             roundTime = time;
         }
     }
